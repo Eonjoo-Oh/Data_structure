c0c63c3d2bf57948c2abeaeeb28ebb33300d54a2
Eonjoo Oh <eoh@cx2r2s6.42seoul.kr>
2023-07-06 17:13:50 +0900

    graph done

--- graph/graph3.c
+++ graph/graph3.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_VERTEX 425
 #define MAX_NAME 20
 
 typedef struct s_store {
-	char			name[MAX_NAME];
+	char			*name;
 	struct s_store	*next;
 	int				nearest;
 }	t_store;
@@ -15,6 +16,19 @@ typedef struct s_graph {
 	t_store	**store;
 }	t_graph;
 
+t_store *init_inner_store()
+{
+	t_store	*store;
+
+	store = (t_store *)malloc(sizeof(t_store) * 1);
+	store->name = (char *)malloc(sizeof(char) * (MAX_NAME + 1));
+	store->name[0] = '\0';
+	store->next = NULL;
+	store->nearest = -1;
+
+	return (store);
+}
+
 t_store	**init_store(void)
 {
 	t_store	**store;
@@ -22,9 +36,7 @@ t_store	**init_store(void)
 	store = (t_store **)malloc(sizeof(t_store *) * MAX_VERTEX);
 	for(int i = 0; i < MAX_VERTEX; i++)
 	{
-		store[i]->name[0] = '\0';
-		store[i]->next = NULL;
-		store[i]->nearest = -1;
+		store[i] = init_inner_store();
 	}
 	return (store);
 }
@@ -45,7 +57,7 @@ void	init_graph(t_graph *graph)
 		}
 		i++;
 	}
-	graph->store = init_store;
+	graph->store = init_store();
 }
 
 int		update_near(t_graph *graph, int num)
@@ -56,15 +68,23 @@ int		update_near(t_graph *graph, int num)
 	int	new_near;
 
 	i = 0;
-	near = -1;
+	near = graph->store[num]->nearest;
 	while (i < MAX_VERTEX)
 	{
 		if (i == num)
+		{
+			i++;
 			continue ;
+		}
 		if (graph->map[i][i] > 0)
 		{
 			if (near == -1)
 				near = i;
+			else if (near == i)
+			{
+				i++;
+				continue;
+			}
 			else
 			{
 				old_near = num - near;
@@ -82,34 +102,27 @@ int		update_near(t_graph *graph, int num)
 	return (near);
 }
 
-int	get_old_near(t_graph *graph, int num)
-{
-	for(int i = 0; i < MAX_VERTEX; i++)
-	{
-		if (i == num)
-			continue;
-		if (graph->map[num][i] == 1)
-			return (i);
-	}
-}
 void	update_nearest(t_graph *graph, int num)
 {
 	int	near;
 	int	old_near;
 
-	near = update_near;
+	near = update_near(graph, num);
+	old_near = graph->store[num]->nearest;
 	if (near == -1)
 		return;
-	old_near = get_old_near;
-	if (near == old_near)
+	if (old_near == near)
 		return ;
-	graph->map[old_near][num] = 0;
-	graph->map[num][old_near] = 0;
-	graph->map[near][num] = 1;
-	graph->map[num][near] = 1;
-	graph->store[num]->nearest = near;
-	graph->store[near]->nearest = num;
-}
+	else
+	{
+		graph->map[old_near][num] = 0;
+		graph->map[num][old_near] = 0;
+		graph->map[near][num] = 1;
+		graph->map[num][near] = 1;
+		graph->store[num]->nearest = near;
+		graph->store[near]->nearest = num;
+	}
+}//가장 가까운 지점을 찾아서 update해주는 함수
 
 void	add_store(t_graph	*graph)
 {
@@ -117,19 +130,26 @@ void	add_store(t_graph	*graph)
 	char	str[20];
 	t_store	*store;
 
-	printf("추가하려는 지역 번호를 입력하세요\n");
+	printf("\n************  최애 가게 등록하기!  *************\n\n");
+	printf("추가하려는 <지역 번호>를 입력하세요\n");
 	scanf("%d", &num);
-
-	printf("추가하려는 가게 이름을 입력하세요\n");
-	scanf("%s", str);
 	store = graph->store[num];
-	while (store->name[0] != '\0')
+
+	printf("\n추가하려는 <가게 이름>을 입력하세요\n");
+	scanf("%s", str);
+	if (store->name[0] == '\0')
+		strcpy(store->name, str);
+	else
 	{
+		while (store->next != NULL)
+		{
+			store = store->next;
+		}
+		store->next = init_inner_store();
 		store = store->next;
+		strcpy(store->name, str);
 	}
-	strcpy(store, str);
-	graph->map[num][num]++;
-	//자기자신과 연결되는 경우는 없으니 안에 몇개가 들었는지 flag로 씀
+	graph->map[num][num]++; //자기 자신과 이어진 것은 자신이 가지고 있는 가게의 개수를 의미함
 	update_nearest(graph, num);
 }
 
@@ -139,48 +159,77 @@ void	find_nearest(t_graph *graph)
 	int		near;
 	t_store	*store;
 
-	printf("당신이 있는 지역을 입력하세요\n");
+	printf("\n*********  내 주변 최애 가게 찾기!  **********\n\n");
+	printf("당신이 있는 지역을 입력하세요\n\n");
 	scanf("%d", &num);
 
-	printf("당신의 지역에 있는 가게는\n");
+	printf("당신의 지역에 있는 가게는\n\n");
 	if (graph->store[num]->name[0] == '\0')
-		printf("없습니다!\n");
+		printf("\n없습니다!\n\n");
 	else
 	{
 		store = graph->store[num];
-		while (store->name[0] != '\0')
+		while (store != NULL)
 		{
 			printf("%s\n", store->name);
 			store = store->next;
 		}
-		printf("입니다\n\n");
+		printf("입니다\n");
 	}
 
-	printf("가장 가까운 다른 동네번호는 ");
+	printf("\n가장 가까운 다른 동네번호는 \n");
 	if (graph->store[num]->nearest == -1)
-		printf("없습니다!\n");
+		update_nearest(graph, num);
+	if (graph->store[num]->nearest == -1)
+	printf("없습니다!\n\n");
 	else
 	{
 		near = graph->store[num]->nearest;
 		printf("%d번 입니다\n", near);
 		printf("이 지역에 있는 가게는 \n");
 		store = graph->store[near];
-		while (store->name[0] != '\0')
+		if (store->name == NULL)
+		{
+			printf("없습니다!\n\n");
+			return ;
+		}
+		while (store != NULL)
 		{
 			printf("%s\n", store->name);
 			store = store->next;
 		}
-		printf("입니다\n\n");
+		printf("입니다\n");
 	}
 }
 
 int	main(void)
 {
 	t_graph	graph;
+	int		num;
 
+	printf("\n안녕하세요 어떤 서비스를 이용하시겠습니까?\n\n");
+	printf("1. 최애 가게 등록하기\n\n2. 내 주변 최애 가게 보기\n\n3.Exit\n\n");
+	scanf("%d", &num);
 	init_graph(&graph);
-	add_store(&graph);
-	find_nearest(&graph);
+	while (1)
+	{
+		if (num == 1)
+			add_store(&graph);
+
+		else if (num == 2)
+			find_nearest(&graph);
+		else if (num == 3)
+		{
+			printf("bye bye\n");
+			exit(0);
+		}
+		else
+			printf("잘못입력하셨습니다. 다시 입력해주세요\n\n");
+		printf("\n\n********************완료!*********************\n");
+		printf("\n\n어떤 서비스를 이용하시겠습니까?\n");
+		printf("1. 최애 가게 등록하기\n2. 내 주변 최애 가게 보기\n3.Exit\n\n");
+		scanf("%d", &num);
+	}
 }
 
 /*int	get_near(t_graph *graph, int num)
